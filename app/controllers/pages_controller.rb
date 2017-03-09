bf97d3a696c19fb7b2c3c3a4c4c9e49b2170e3a6
Danish Mughal <mughal.danish@gmail.com>
2017-03-08 21:00:54 -0800

    Solution for guessing game + URL parameters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,21 @@ class PagesController < ApplicationController
     @message = params[:message]
   end
 
+  def game
+    number = 36
+    @guess = params[:guess].to_i
+
+    if @guess == number
+      @status = "You win!"
+    elsif @guess < number
+      @status = "You guessed too low. Guess higher!"
+    elsif @guess > number
+      @status = "You guessed too high! Guess lower."
+    end
+  end
+
+  def url_params
+    @message = params[:message]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/query_params" => "pages#query_params"
+  get "/game" => "pages#game"
 
+  get "/url_params/:message" => "pages#url_params"
 end
